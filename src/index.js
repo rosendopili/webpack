f63afa612725a6092219f6f690a43879afd1e66d
rosendopili <rosendopili@gmail.com>
2020-07-27 22:37:25 -0400

    added babel.config.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 console.log("Hello, Webpack!")
 
 let someStr = "I coded. I saved. I bundled.";
+
+const numbers = [2, 3, 5];
+
+// Arrow function. How will Internet Explorer cope with it?
+const doubledNumbers = numbers.map(number => number * 2); 
+
+console.log(doubledNumbers); // 4, 6, 10
--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,22 @@ const path = require("path"); // connect path to webpack config
 // we specified the location where Webpack will start bundling - it's the index.js file inside the src folder
 
 module.exports = {
-    entry: { main: "./src/index.js" }, 
+    entry: { main: "./src/index.js" },
     output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "main.js"
-    }
-}
-
-// specified and named the file in which all the js code will be collected
-
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js"
+  },
+    module: {
+    rules: [ // this is an array of rules
+      // add an object containing rules for Babel to it
+      {
+        // a regular expression that searches for all js files
+        test: /\.js$/,
+        // all files must be processed by babel-loader
+        loader: "babel-loader",
+        // exclude the node_modules folder, we don't need to process files in it
+        exclude: "/node_modules/"
+      }
+      ]
+  }
+};
