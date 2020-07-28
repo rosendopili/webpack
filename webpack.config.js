fa0dbef5c35ccbde086cf22972217dcb4d49c701
rosendopili <rosendopili@gmail.com>
2020-07-27 23:07:59 -0400

    added html-loader and file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -9,18 +9,26 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js"
   },
-    module: {
-    rules: [ // this is an array of rules
-      // add an object containing rules for Babel to it
+  module: {
+    rules: [
       {
-        // a regular expression that searches for all js files
         test: /\.js$/,
-        // all files must be processed by babel-loader
         loader: "babel-loader",
-        // exclude the node_modules folder, we don't need to process files in it
         exclude: "/node_modules/"
-      }
-      ]
+      },
+      // add the rule for processing files
+      {
+        // this regular expression will search for files with the following extensions
+        test: /\.(png|svg|jpg|gif|woff2)$/,
+        // file-loader should be used when processing those files
+        loader: "file-loader"
+      },
+      // add the rule for working with HTML in a similar way
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+      },
+    ]
   }, 
   plugins: [new HtmlWebpackPlugin({
     template: "./src/index.html" // path to our index.html file
